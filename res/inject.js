8be3b70492deaaed5545e0b7764cc66b7930340d
Alan S. Tennent <tennent4@gmail.com>
2014-11-13 20:37:48 -0800

    Implemented Saving and Testing
    Still need to actually use the saved data...partially implemented.

--- res/inject.js
+++ res/inject.js
@@ -1,1 +1,22 @@
-console.log("injecting");
+chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    var currentURL = document.URL;
+    var matches = [];
+    for (index in allKeys) {
+    	var regexStr = allKeys[index];
+    	if (currentURL.match(regexStr) !== null) {
+    		matches.append(regexStr);
+    		console.log("Matched rule: " + regexStr);
+    	}
+
+    }
+
+    chrome.storage.sync.get(matches, function(x) {
+		console.log(x);
+	});
+});
+
+function applyStyleString(cssStr) {
+	console.log(cssStr);
+	chrome.tabs.insertCSS( {'code':cssStr} );
+}
--- res/popup.js
+++ res/popup.js
@@ -1,15 +1,48 @@
-function onload() {
+{
+	function onload() {
+		chrome.tabs.query({'active': true}, function (tabs) {
+			const currentURL = tabs[0].url;
+			const domainRegex = /^.*\/\/.*?\//;
+			const currentDomain = currentURL.match(domainRegex);
+			const regexTextfield = document.getElementById("new-rule-regex")
+			regexTextfield.value = currentDomain + "*";
+			console.log(tabs[0]);
+		});
 
-	var url = "";
-	chrome.tabs.query({'active': true}, function (tabs) {
-    	url = tabs[0].url;
-    	console.log(url)
+		document.getElementById("test-btn").addEventListener("click", function(){
+			var customCSS = document.getElementById("new-rule-content").value;
+		    testStyleString(customCSS);
+		});
+
+		document.getElementById("save-btn").addEventListener("click", function(){
+			var customCSS = document.getElementById("new-rule-content").value;
+			var regex = document.getElementById("new-rule-regex").value;
+		    saveStyleString(customCSS, regex);
+		});
+
+	}
+
+	document.addEventListener('DOMContentLoaded', function() {
+	  onload();
 	});
-	const currentDomain = document.domain;
-	document.getElementById("new-rule-regex").value = currentDomain + "/*";
-	console.log("test");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  onload();
-});
+function testStyleString(cssStr) {
+	console.log(cssStr);
+	chrome.tabs.insertCSS( {'code':cssStr} );
+}
+
+function saveStyleString(cssStr, regex) {
+	var kvp = {};
+	kvp[regex] = cssStr;
+	// Save it using the Chrome extension storage API.
+    chrome.storage.sync.set(kvp, function() {
+      // Notify that we saved.
+      alert('StyleSheet saved');
+    });
+}
+
+function clearStorage() {
+	StorageArea.clear();
+}
+clearStorage();
