5ed3e9f06b5f4ef548adaf9062c6f3205788a693
Alan Tennent <tennent4@gmail.com>
2014-11-18 18:51:08 -0800

    More work toward saving data
    Still currently broken. Just committing to share code between
    computers..

--- res/inject.js
+++ res/inject.js
@@ -3,20 +3,22 @@ chrome.storage.sync.get(null, function(items) {
     var currentURL = document.URL;
     var matches = [];
     for (index in allKeys) {
-    	var regexStr = allKeys[index];
+        var regexStr = allKeys[index];
     	if (currentURL.match(regexStr) !== null) {
-    		matches.append(regexStr);
-    		console.log("Matched rule: " + regexStr);
+    		matches.push(regexStr);
     	}
 
     }
-
-    chrome.storage.sync.get(matches, function(x) {
-		console.log(x);
+    chrome.storage.sync.get(matches, function(matchingRules) {
+        for (rule in matchingRules) {
+            var cssStr = matchingRules[rule];
+            applyStyleString(cssStr);
+        }
 	});
 });
 
-function applyStyleString(cssStr) {
-	console.log(cssStr);
-	chrome.tabs.insertCSS( {'code':cssStr} );
+function applyStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
 }
--- res/popup.js
+++ res/popup.js
@@ -1,31 +1,43 @@
-{
-	function onload() {
-		chrome.tabs.query({'active': true}, function (tabs) {
-			const currentURL = tabs[0].url;
-			const domainRegex = /^.*\/\/.*?\//;
-			const currentDomain = currentURL.match(domainRegex);
-			const regexTextfield = document.getElementById("new-rule-regex")
-			regexTextfield.value = currentDomain + "*";
-			console.log(tabs[0]);
-		});
 
-		document.getElementById("test-btn").addEventListener("click", function(){
-			var customCSS = document.getElementById("new-rule-content").value;
-		    testStyleString(customCSS);
-		});
+// Document On-load function
+document.addEventListener('DOMContentLoaded', function() {
+	chrome.tabs.query({'active': true}, function (tabs) {
+		
+		// Fill in the a default regex based on the current url.
+		const regexTextfield = document.getElementById("new-rule-regex");
+		const currentURL = tabs[0].url;
+		const domainRegex = /^.*\/\/.*?\//; // May not work with everything, but it's good enough.
+		const currentDomain = currentURL.match(domainRegex);
+		regexTextfield.value = currentDomain + "*"; // Asterisk to match trailing stuff.
 
-		document.getElementById("save-btn").addEventListener("click", function(){
-			var customCSS = document.getElementById("new-rule-content").value;
-			var regex = document.getElementById("new-rule-regex").value;
-		    saveStyleString(customCSS, regex);
-		});
 
-	}
+		function addRule(regexStr) {
+			var node = document.createElement('iframe');
+			node.src = "rule.html";
+		    node.innerHTML = regexStr;
+		    document.body.appendChild(node);
+		}
 
-	document.addEventListener('DOMContentLoaded', function() {
-	  onload();
+		// Find existing rules and add them.
+		for (var i = 0 ; i < 3 ; i++ ) {
+			addRule("x" + i)
+		}
+
+
 	});
-}
+
+
+	document.getElementById("test-btn").addEventListener("click", function(){
+		var customCSS = document.getElementById("new-rule-content").value;
+	    testStyleString(customCSS);
+	});
+
+	document.getElementById("save-btn").addEventListener("click", function(){
+		var customCSS = document.getElementById("new-rule-content").value;
+		var regex = document.getElementById("new-rule-regex").value;
+	    saveStyleString(customCSS, regex);
+	});
+});
 
 function testStyleString(cssStr) {
 	console.log(cssStr);
@@ -43,6 +55,6 @@ function saveStyleString(cssStr, regex) {
 }
 
 function clearStorage() {
+	// This might not work.
 	StorageArea.clear();
 }
-clearStorage();
